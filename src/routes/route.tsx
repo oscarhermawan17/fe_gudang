31e0b7a9eefd7ecb9ae5738c64a3dbe38cb1e037
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-06-17 14:32:04 +0800

    FrontEnd - testing routing

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -3,19 +3,19 @@ import { createBrowserRouter } from "react-router-dom"
 import App from "../App"
 import SignUp from "../Pages/SignUpPage/SignUpPage"
 import ErrorPage from "../Pages/ErrorPage/ErrorPage"
-import NotFoundPage from "../Pages/NotFoundPage/NotFoundPage"
-import HomePage from "../Pages/HomePage/HomePage"
-import ProtectedPage from "../Pages/ProtectedPage/ProtectedPage"
+// import NotFoundPage from "../Pages/NotFoundPage/NotFoundPage"
+// import HomePage from "../Pages/HomePage/HomePage"
+// import ProtectedPage from "../Pages/ProtectedPage/ProtectedPage"
 
 // Protected Route
-const protectedRoutes = [
-  { path: "/home", element: <HomePage /> },
-  { path: "/x", element: <>test</> },
-]
+// const protectedRoutes = [
+//   { path: "/home", element: <HomePage /> },
+//   { path: "/x", element: <>test</> },
+// ]
 
 const router = createBrowserRouter([
   {
-    path: "/login",
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
   },
@@ -23,21 +23,21 @@ const router = createBrowserRouter([
     path: "/signup",
     element: <SignUp />,
   },
-  ...protectedRoutes.map((route) => ({
-    path: route.path,
-    element: <ProtectedPage />,
-    children: [
-      {
-        path: "",
-        element: route.element,
-        errorElement: <ErrorPage />,
-      },
-    ],
-  })),
-  {
-    path: '*',
-    element: <NotFoundPage />,
-  },
+  // ...protectedRoutes.map((route) => ({
+  //   path: route.path,
+  //   element: <ProtectedPage />,
+  //   children: [
+  //     {
+  //       path: "",
+  //       element: route.element,
+  //       errorElement: <ErrorPage />,
+  //     },
+  //   ],
+  // })),
+  // {
+  //   path: '*',
+  //   element: <NotFoundPage />,
+  // },
 ])
 
 export default router
