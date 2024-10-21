5a5566b895b2a3e37682392ddd365a7c6c04f632
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-10-21 18:18:37 +0700

    FrontEnd - upp dist

--- src/utils/api/auth/login.ts
+++ src/utils/api/auth/login.ts
@@ -5,7 +5,7 @@ import axios from 'axios'
 export const userLogin = async (value: LoginPayload) => {
   try {
     // const respons = await APIClient.post<LoginResponse>(`/api/${import.meta.env.VITE_BE_API_VERSION}/auth/login`, value);
-    const respons = await axios.post<LoginResponse>(`http://localhost:8000/api/${import.meta.env.VITE_BE_API_VERSION}/auth/login`, value);
+    const respons = await axios.post<LoginResponse>(`${import.meta.env.VITE_BE_API_URL}/api/${import.meta.env.VITE_BE_API_VERSION}/auth/login`, value);
 
     return respons.data;
   } catch (err) {
