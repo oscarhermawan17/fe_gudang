ec2fd8955805b24731d1c326da3078fb18fbfd09
Oscar Hermawan <oscar.hermawan90@gmail.com>
2024-12-08 02:11:22 +0700

    Form Validation

--- src/Components/ModalForm.component.tsx
+++ src/Components/ModalForm.component.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as z from 'zod';
+import React, { useState } from 'react';
 import {
   TextField, Button, Box, FormControl, Select, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions
 } from '@mui/material';
@@ -21,36 +18,45 @@ interface ModalFormProps {
   title: string;
   formsInput: FormInputType[];
   onSubmit: (data: { [key: string]: string }) => void;
-  onChange: () => void
+  onChange: (args: { value: string; entity: string }) => void;
 }
 
-// Dynamic Zod Schema based on formsInput
-const generateSchema = (formsInput: FormInputType[]) => {
-  const schemaObj: any = {};
-  
-  formsInput.forEach((input) => {
-    if (input.required) {
-      schemaObj[input.name] = z.string().min(1, `${input.label} is required`);
-    } else {
-      schemaObj[input.name] = z.string().optional();
-    }
-  });
-  
-  return z.object(schemaObj);
-};
-
-const ModalForm: React.FC<ModalFormProps> = ({ open, onChange, onClose, title, formsInput, onSubmit }) => {  
-  const schema = generateSchema(formsInput);
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: zodResolver(schema),
-    defaultValues: formsInput.reduce((acc, curr) => {
+const ModalForm: React.FC<ModalFormProps> = ({ open, onClose, title, formsInput, onSubmit, onChange }) => {
+  const [formValues, setFormValues] = useState<{ [key: string]: string }>(
+    formsInput.reduce((acc, curr) => {
       acc[curr.name] = '';
       return acc;
     }, {} as { [key: string]: string })
-  });
+  );
 
-  const onSubmitForm: SubmitHandler<{ [key: string]: string }> = (data) => {
-    onSubmit(data);
+  const [formErrors, setFormErrors] = useState<{ [key: string]: string }>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | { value: unknown }>, name: string) => {
+    const value = e.target.value as string;
+    setFormValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    onChange({ value, entity: name });
+  };
+
+  const validateForm = () => {
+    const errors: { [key: string]: string } = {};
+    formsInput.forEach((input) => {
+      if (input.required && !formValues[input.name]) {
+        errors[input.name] = `${input.label} is required`;
+      }
+    });
+    return errors;
+  };
+
+  const handleSubmit = () => {
+    const errors = validateForm();
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      return;
+    }
+    onSubmit(formValues);
     onClose();
   };
 
@@ -58,97 +64,48 @@ const ModalForm: React.FC<ModalFormProps> = ({ open, onChange, onClose, title, f
     <Dialog open={open} onClose={onClose} fullWidth maxWidth="xs">
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>
-        <form onSubmit={handleSubmit(onSubmitForm)}>
-          <FormControl fullWidth>
-            {formsInput.map((input) => {
-              if (input.type === 'percentage') {
-                return (
-                  <Box key={input.name} sx={{ marginBottom: 2 }}>
-                    <TextField
-                      type="number"
-                      size={'small'}
-                      inputProps={{ min: 0, max: 100 }}
-                      fullWidth
-                      label={input.label}
-                      disabled={input.disabled}
-                      {...register(input.name)}
-                      error={!!errors[input.name]}
-                      helperText={errors[input.name]?.message as string}
-                      value={input.value}
-                      onChange={(e) => onChange({ value: e.target.value, entity: input.name })}
-                    />
-                  </Box>
-                );
-              } else if (input.type === 'number') {
-                return (
-                  <Box key={input.name} sx={{ marginBottom: 2 }}>
-                    <TextField
-                      type="number"
-                      size={'small'}
-                      fullWidth
-                      label={input.label}
-                      disabled={input.disabled}
-                      {...register(input.name)}
-                      error={!!errors[input.name]}
-                      helperText={errors[input.name]?.message as string}
-                      value={input.value}
-                      onChange={(e) => onChange({ value: e.target.value, entity: input.name })}
-                    />
-                  </Box>
-                );
-              } else if (input.type === 'text') {
-                return (
-                  <Box key={input.name} sx={{ marginBottom: 2 }}>
-                    <TextField
-                      size={'small'}
-                      fullWidth
-                      label={input.label}
-                      disabled={input.disabled}
-                      {...register(input.name)}
-                      error={!!errors[input.name]}
-                      helperText={errors[input.name]?.message as string}
-                      value={input.value}
-                      onChange={(e) => onChange({ value: e.target.value, entity: input.name })}
-                    />
-                  </Box>
-                );
-              } else if (input.type === 'dropdown' && input.options) {
-                return (
-                  <Box key={input.name} sx={{ marginBottom: 2 }}>
-                    <Select
-                      fullWidth
-                      {...register(input.name)}
-                      error={!!errors[input.name]}
-                      displayEmpty
-                      size={'small'}
-                      value={input.value}
-                      onChange={(e) => onChange({ value: e.target.value, entity: input.name })}
-                    >
-                      <MenuItem disabled value="">
-                        Select {input.label}
-                      </MenuItem>
-                      {input.options?.map((option) => (
-                        <MenuItem key={option.id} value={option.id}>
-                          {option.value}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                    {errors[input.name] && (
-                      <Box color="error.main" mt={1}>
-                        {errors[input.name]?.message as string}
-                      </Box>
-                    )}
-                  </Box>
-                );
-              }
-              return null;
-            })}
-          </FormControl>
-        </form>
+        <FormControl fullWidth>
+          {formsInput.map((input) => (
+            <Box key={input.name} sx={{ marginBottom: 2 }}>
+              {input.type === 'text' || input.type === 'number' || input.type === 'percentage' ? (
+                <TextField
+                  type={input.type === 'percentage' ? 'number' : input.type}
+                  size="small"
+                  fullWidth
+                  label={input.label}
+                  disabled={input.disabled}
+                  inputProps={input.type === 'percentage' ? { min: 0, max: 100 } : undefined}
+                  value={formValues[input.name]}
+                  onChange={(e) => handleChange(e, input.name)}
+                  error={!!formErrors[input.name]}
+                  helperText={formErrors[input.name]}
+                />
+              ) : input.type === 'dropdown' && input.options ? (
+                <Select
+                  fullWidth
+                  displayEmpty
+                  size="small"
+                  value={formValues[input.name]}
+                  onChange={(e) => handleChange(e, input.name)}
+                  error={!!formErrors[input.name]}
+                >
+                  <MenuItem disabled value="">
+                    Select {input.label}
+                  </MenuItem>
+                  {input.options.map((option) => (
+                    <MenuItem key={option.id} value={option.id}>
+                      {option.value}
+                    </MenuItem>
+                  ))}
+                </Select>
+              ) : null}
+            </Box>
+          ))}
+        </FormControl>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={handleSubmit(onSubmitForm)} variant="contained" color="primary">
+        <Button onClick={handleSubmit} variant="contained" color="primary">
           Submit
         </Button>
       </DialogActions>
